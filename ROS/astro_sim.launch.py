502c0f07e83f00ec12b098c3fd3ff613558da406
Saiedeh (Saia) Akbari <akbaris@ufl.edu>
2025-12-17 17:42:28 -0500

    Add files via upload

---
+++ ROS/astro_sim.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='lyapunov_adaptive_transformer',
+            namespace='astro_sim',
+            executable='lyapunov_adaptive_transformer',
+            parameters=[os.path.join(get_package_share_directory('px4_telemetry'), 'param', 'park_coordinates.yaml')],
+            name='lyapunov_adaptive_transformer_node'
+        )
+    ])
